fde08be6ec37fe8da25b143c79186e70a1aea603
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2025-11-07 10:52:13 -0500

    add Ship class

---
+++ src/model/Ship.js
@@ -0,0 +1,39 @@
+class Ship {
+  #length;
+  #hits;
+  #sunk;
+  constructor(length = 1, hits = 0, sunk = false) {
+    this.#length = length;
+    this.#hits = hits;
+    this.#sunk = sunk;
+  }
+  get length() {
+    return this.#length;
+  }
+  set length(length) {
+    this.#length = length;
+  }
+  get hits() {
+    return this.#hits;
+  }
+  set hits(hits) {
+    this.#hits = hits;
+  }
+  get sunk() {
+    return this.#sunk;
+  }
+  set sunk(sunk) {
+    this.#sunk = sunk;
+  }
+  //METHODS
+  hit() {
+    if (!this.isSunk()) this.#hits++;
+  }
+  isSunk() {
+    if (this.#hits === this.#length) {
+      this.#sunk = true;
+    }
+    return this.#sunk;
+  }
+}
+export default Ship;
